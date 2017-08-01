23a11aa5ba59a696875191a1d5c79b5bb1846758
oisincoveney <oisincoveney@gmail.com>
2017-08-01 10:19:29 -0700

    Committed finished product

---
+++ clarifaivision-core/src/main/java/org/nuxeo/clarifaivision/ClarifaiHandler.java
@@ -0,0 +1,113 @@
+package org.nuxeo.clarifaivision;
+
+import clarifai2.api.ClarifaiBuilder;
+import clarifai2.api.ClarifaiClient;
+import clarifai2.dto.input.ClarifaiInput;
+import clarifai2.dto.model.output.ClarifaiOutput;
+import clarifai2.dto.prediction.Concept;
+import org.nuxeo.ecm.core.api.CoreSession;
+import org.nuxeo.ecm.core.api.DocumentModel;
+import org.nuxeo.ecm.core.api.NuxeoException;
+import org.nuxeo.ecm.core.api.impl.blob.FileBlob;
+import org.nuxeo.ecm.platform.tag.TagService;
+import org.nuxeo.runtime.api.Framework;
+
+import java.io.File;
+import java.util.List;
+
+
+/**
+ * (C) Copyright 2017 Nuxeo (http://nuxeo.com/) and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Contributors:
+ * Oisin Coveney
+ *
+ *
+ * A wrapper for handling Clarifai's functions, including its image prediction service.
+ */
+class ClarifaiHandler
+{
+
+    //The API Key for accessing Clarifai's services, set through the nuxeo.conf configuration file
+    private String API_KEY;
+
+    /**
+     * Constructor -> initializes the API_KEY variable from the Nuxeo conf file, or throws
+     * an exception if an API key cannot be found.
+     */
+    ClarifaiHandler ()
+    {
+        API_KEY = Framework.getProperty("org.nuxeo.clarifaivision.key");
+        if ( API_KEY == null || API_KEY.equals("") )
+        {
+            throw new NuxeoException("No API Key has been provided. Please add the key to nuxeo.conf using the" +
+                                             "org.nuxeo.clarifaivision.key parameter");
+        }
+    }
+
+    /**
+     * Tags a DocumentModel picture object with tags retrieved from Clarifai's image prediction
+     * service, which takes in pictures and returns tags based on machine learning algorithms.
+     *
+     * @param doc -> a DocumentModel Picture object to tag with Clarifai
+     */
+    void tag ( DocumentModel doc )
+    {
+        if ( doc == null )
+            return;
+
+        //Gets the small image preview to be sent to Clarifai to save time
+        FileBlob blob;
+
+        try
+        {
+            blob = (FileBlob) doc.getPropertyObject("picture", "views").get(1).get("content").getValue();
+        }
+        catch ( Throwable e )
+        {
+            return;
+        }
+
+        File file = blob.getFile();
+
+        if ( file == null )
+            return;
+
+
+        //Calls the Clarifai client to receive tags
+        ClarifaiClient client = new ClarifaiBuilder(API_KEY).buildSync();
+        List <ClarifaiOutput <Concept>> rep = client.getDefaultModels().generalModel().predict().withInputs(
+                ClarifaiInput.forImage(file)).executeSync().get();
+
+        //Call the CoreSession from the DocumentModel given to add tags to the image
+        TagService  service = Framework.getLocalService(TagService.class);
+        String      id      = doc.getId();
+        CoreSession session = doc.getCoreSession();
+
+        //Add tags to image with nested forEach loop
+        rep.forEach(out -> out.data().forEach(con -> service.tag(session, id, con.name(), "Administrator")));
+    }
+
+
+    String getAPIKey ()
+    {
+        return API_KEY;
+    }
+
+    void setAPIKey ( String apiKey )
+    {
+        this.API_KEY = API_KEY;
+    }
+}
---
+++ clarifaivision-core/src/main/java/org/nuxeo/clarifaivision/ClarifaiTaggerOperation.java
@@ -0,0 +1,56 @@
+package org.nuxeo.clarifaivision;
+
+import org.nuxeo.ecm.automation.core.Constants;
+import org.nuxeo.ecm.automation.core.annotations.Operation;
+import org.nuxeo.ecm.automation.core.annotations.OperationMethod;
+import org.nuxeo.ecm.core.api.DocumentModel;
+import org.nuxeo.ecm.core.api.NuxeoException;
+
+
+/**
+ * (C) Copyright 2017 Nuxeo (http://nuxeo.com/) and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Contributors:
+ * Oisin Coveney
+ */
+@Operation ( id = ClarifaiTaggerOperation.ID, category = Constants.CAT_DOCUMENT, label = "Clarifai Tagger Operation",
+        description = "Tags pictures using Clarifai's image prediction service (https://clarifai.com/)" )
+public class ClarifaiTaggerOperation
+{
+
+    static final         String ID           = "Document.ClarifaiTaggerOperation";
+    private static final String PICTURE_TYPE = "Picture";
+
+    /**
+     * @param doc -> a Nuxeo DocumentModel object called from an Operation chain
+     *
+     * @return a DocumentModel object which contains a Picture with new tags from Clarifai's image prediction service,
+     * or the DocumentModel object that was inputted if the input was invalid.
+     *
+     * @throws NuxeoException if the DocumentModel is not an image
+     */
+    @OperationMethod
+    public DocumentModel run ( DocumentModel doc ) throws NuxeoException
+    {
+        if ( !doc.getType().equals(PICTURE_TYPE) )
+            throw new NuxeoException("Operation only works with " + PICTURE_TYPE + " document types");
+        else
+        {
+            ClarifaiHandler handler = new ClarifaiHandler();
+            handler.tag(doc);
+            return doc;
+        }
+    }
+}
